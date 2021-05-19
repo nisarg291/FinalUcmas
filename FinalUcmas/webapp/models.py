5994f6d8dea891e4e04b65949b1ba4154827bee2
Nisarg291 <59527108+nisarg291@users.noreply.github.com>
2021-05-19 20:27:01 +0530

    Add files via upload

--- FinalUcmas/webapp/models.py
+++ FinalUcmas/webapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from PIL import Image
 # Create your models here.
+class Student(models.Model):
+    stu_id = models.AutoField(primary_key=True)
+    StudentUserName=models.CharField(max_length=50,default="")
+    StudentFirstName=models.CharField(max_length=50)
+    StudentMiddleName=models.CharField(max_length=50)
+    StudentLastName=models.CharField(max_length=50)
+    # StudentImage = models.FileField(default="default.jpg",upload_to='student/images')
+    BirthDate = models.CharField(max_length=50,default="")
+    Gender=models.CharField(max_length=10)
+    StudentEmail=models.EmailField(max_length=254,default="")
+    std=models.IntegerField()
+    Enroll=models.IntegerField()
+    ucmaslevel=models.IntegerField(default=1)
+    SchoolName=models.CharField(max_length=100)
+    Hobbies=models.CharField(max_length=200)
+    Refference=models.CharField(max_length=100)
+    Ressidence=models.TextField()
+    FatherName = models.CharField(max_length=70)
+    FatherQualification=models.CharField(max_length=200)
+    FatherOccupation=models.CharField(max_length=100)
+    FatherEmail=models.EmailField(max_length=254,default="")
+    FatherContact1=models.CharField(max_length=10)
+    FatherContact2=models.CharField(max_length=10)
+    MotherName = models.CharField(max_length=70)
+    MotherQualification=models.CharField(max_length=100)
+    MotherOccupation=models.CharField(max_length=100)
+    MotherEmail=models.EmailField(max_length=254,default="")
+    MotherContact1=models.CharField(max_length=10)
+    MotherContact2=models.CharField(max_length=10)
+    date=models.DateField(auto_now_add=True,null=True)
+    
+    def __str__(self):
+        return self.StudentFirstName+" "+self.StudentMiddleName+" "+self.StudentLastName
--- FinalUcmas/webapp/views.py
+++ FinalUcmas/webapp/views.py
@@ -1,37 +1,36 @@
 from django.shortcuts import render,HttpResponse,redirect
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
+# import pyttsx3 #pip install pyttsx3
+# import speech_recognition as sr #pip install speechRecognition
 from django.contrib import messages
 import datetime
 # from bs4 import BeautifulSoup
 import requests
-import pyaudio
-import playsound #pip install wikipedia
 # import webbrowser
+from django.contrib.auth.decorators import login_required
 import os
 # import smtplib
 import time
-import webbrowser
 import random
 
-from gtts import gTTS
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
-engine.setProperty('rate', 200)
-def speak(text):
-    tts=gTTS(text=text,lang="en")
-    fileName="voice.mp3"
-    tts.save(fileName)
-    playsound.playsound(fileName)
+# from gtts import gTTS
+# engine = pyttsx3.init('sapi5')
+# voices = engine.getProperty('voices')
+# print(voices[1].id)
+# engine.setProperty('voice', voices[0].id)
+# engine.setProperty('rate', 200)
+# def speak(text):
+#     tts=gTTS(text=text,lang="en")
+#     fileName="voice.mp3"
+#     tts.save(fileName)
+#     playsound.playsound(fileName)
 
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-# Create your views here.
+# def speak(audio):
+#     engine.say(audio)
+#     engine.runAndWait()
+# # Create your views here.
 
 # for listening and flash menu page
+@login_required
 def listening_flash_menu(request):
     return render(request,'listening_flash_menu.html')
 
@@ -47,6 +46,12 @@ def gallary(request):
 def levels(request):
     return render(request,'levels.html')
 
+@login_required
+def info(request):
+    return render(request,'info.html')
+
+def profile(request):
+    return render(request, 'profile.html')
 # for aboutus page
 def aboutus(request):
     return render(request,'aboutus.html')
@@ -83,34 +88,42 @@ def login(request):
     return render(request,'login.html') 
 
 # for addition Option Page for listening 
+@login_required
 def addition(request):
     return render(request,'Addition.html')
 
 # for multiplication Option Page for listening 
+@login_required
 def multiplication(request):
     return render(request,'Multiplication.html')
 
 # for division Option Page for listening 
+@login_required
 def division(request):
     return render(request,'division.html')
 
-# for listening practice Page 
+# for listening practice Page
+@login_required 
 def listening(request):
     return render(request,'listening.html')
 
 # for flash practice Page
+@login_required
 def flash(request):
     return render(request,'flash.html')
 
 # for addition Option Page for flash card
+@login_required
 def flash_addition(request):
     return render(request,'flash_addition.html')
 
 # for multiplication Option Page for flash card
+@login_required
 def flash_multiplication(request):
     return render(request,'flash_multiplication.html')
 
 # for division Option Page for flash card
+@login_required
 def flash_division(request):
     return render(request,'flash_division.html')
 
@@ -130,90 +143,92 @@ def generate_flash_addition(request):
     questions=[]
     ans1=[]
     if request.method=="POST":
-        no_of_digits=int(request.POST.get("digits"))
-        no_of_rows=int(request.POST.get("rows"))
-        no_of_questions=int(request.POST.get("questions"))
-        questions_speed=int(request.POST.get('questions_speed'))
-        print(no_of_digits)
-        print(questions_speed)
-        print(no_of_questions)
-        print(no_of_rows)
-        for que in range(0,no_of_questions):
-            question=[]
-            ans=[]
-            j=0
-            for i in range(0,no_of_rows//5):
-                rr=random.randrange(0,3);
-                if rr==0:
-                    rand=random.randrange(2,5);
-                    for row in range(0,5):
-                        if rand!=2:
-                            if row!=rand:
-                                n=random.randrange(int('1'*no_of_digits),int('9'*no_of_digits))
-                                question.append(n)  
-                            else:
-                                n1=random.randrange(int('6'*no_of_digits),int('9'*no_of_digits))
-                                question.append(-n1) 
-                        if rand==2:
-                            if row==1:
-                                n=random.randrange(int('6'*no_of_digits),int('9'*no_of_digits))
-                                question.append(n) 
-                            elif row!=rand:
-                                n=random.randrange(int('1'*no_of_digits),int('9'*no_of_digits))
-                                question.append(n)  
-                            else:
-                                n1=random.randrange(int('6'*no_of_digits),int('9'*no_of_digits))
-                                if question[j-2]>0 and question[j-1]>0:
-                                    print(question[j-2]+question[j-1])
-                                    if question[j-2]+question[j-1]-n1>=0:
-                                        question.append(-n1) 
-                                    else:
-                                        n1=random.randrange(int('6'*no_of_digits),int(question[j-2]+question[j-1]))
-                                        question.append(-n1)
+        try:
+            no_of_digits=int(request.POST.get("digits"))
+            no_of_rows=int(request.POST.get("rows"))
+            no_of_questions=int(request.POST.get("questions"))
+            questions_speed=int(request.POST.get('questions_speed'))
+            print(no_of_digits)
+            print(questions_speed)
+            print(no_of_questions)
+            print(no_of_rows)
+            for que in range(0,no_of_questions):
+                question=[]
+                ans=[]
+                j=0
+                for i in range(0,no_of_rows//5):
+                    rr=random.randrange(0,3);
+                    if rr==0:
+                        rand=random.randrange(2,5);
+                        for row in range(0,5):
+                            if rand!=2:
+                                if row!=rand:
+                                    n=random.randrange(int('1'*no_of_digits),int('9'*no_of_digits))
+                                    question.append(n)  
                                 else:
-                                    question.append(-n1)
-                        j+=1
-                elif rr==1:
-                # for i in range(0,no_of_rows//5):
-                    rand=random.randrange(1,2);
-                    if rand==2:
-                        rand1=4;
-                    else:
-                        rand1=3;
-                    for row in range(0,5):
-                        if row==int(rand-1) or row==int(rand1-1):
-                            n=random.randrange(int('6'*no_of_digits), int('9'*no_of_digits))
-                            question.append(n)
-                        elif row==rand or row==rand1:
-                            n1=random.randrange(int('1'*no_of_digits), int('6'*no_of_digits))
-                            question.append(-n1)
+                                    n1=random.randrange(int('6'*no_of_digits),int('9'*no_of_digits))
+                                    question.append(-n1) 
+                            if rand==2:
+                                if row==1:
+                                    n=random.randrange(int('6'*no_of_digits),int('9'*no_of_digits))
+                                    question.append(n) 
+                                elif row!=rand:
+                                    n=random.randrange(int('1'*no_of_digits),int('9'*no_of_digits))
+                                    question.append(n)  
+                                else:
+                                    n1=random.randrange(int('6'*no_of_digits),int('9'*no_of_digits))
+                                    if question[j-2]>0 and question[j-1]>0:
+                                        print(question[j-2]+question[j-1])
+                                        if question[j-2]+question[j-1]-n1>=0:
+                                            question.append(-n1) 
+                                        else:
+                                            n1=random.randrange(int('6'*no_of_digits),int(question[j-2]+question[j-1]))
+                                            question.append(-n1)
+                                    else:
+                                        question.append(-n1)
+                            j+=1
+                    elif rr==1:
+                    # for i in range(0,no_of_rows//5):
+                        rand=random.randrange(1,2);
+                        if rand==2:
+                            rand1=4;
                         else:
+                            rand1=3;
+                        for row in range(0,5):
+                            if row==int(rand-1) or row==int(rand1-1):
+                                n=random.randrange(int('6'*no_of_digits), int('9'*no_of_digits))
+                                question.append(n)
+                            elif row==rand or row==rand1:
+                                n1=random.randrange(int('1'*no_of_digits), int('6'*no_of_digits))
+                                question.append(-n1)
+                            else:
+                                n=random.randrange(int('1'*no_of_digits), int('9'*no_of_digits))
+                                question.append(n)
+                            j+=1
+                    elif rr==2:
+                    # for i in range(0,no_of_rows//5):
+                        for row in range(0,5):
                             n=random.randrange(int('1'*no_of_digits), int('9'*no_of_digits))
                             question.append(n)
-                        j+=1
-                elif rr==2:
-                # for i in range(0,no_of_rows//5):
-                    for row in range(0,5):
-                        n=random.randrange(int('1'*no_of_digits), int('9'*no_of_digits))
-                        question.append(n)
-                        j+=1
-            print(question)
-            questions.append(question)
-            sum=0
-            for i in question:
-                # speak(i)
-                sum+=i;
-            # speak("that is?")
-            # time.sleep(10)
-            # ans.append(sum)
-            ans1.append(sum)
+                            j+=1
+                print(question)
+                questions.append(question)
+                sum=0
+                for i in question:
+                    # speak(i)
+                    sum+=i;
+                # speak("that is?")
+                # time.sleep(10)
+                # ans.append(sum)
+                ans1.append(sum)
+                print(ans1)
+                # anss.append(ans)
+            print(questions)
             print(ans1)
-            # anss.append(ans)
-        print(questions)
-        print(ans1)
           
-        return render(request,"generate_flash_addition.html",{'questions':questions,'ans':ans1,'no_of_questions':no_of_questions,'no_of_rows':no_of_rows,'questions_speed':questions_speed})
-
+            return render(request,"generate_flash_addition.html",{'questions':questions,'ans':ans1,'no_of_questions':no_of_questions,'no_of_rows':no_of_rows,'questions_speed':questions_speed})
+        except:
+            return redirect('flash_addition')
 # for addition generator page for listening card   
 def generate_add(request):
     questions=[]
